57d423f7f2a39d41f5183addb55d2bb9d6aeeeac
khushimalik04 <khushi.119018@stu.upes.ac.in>
2026-02-14 00:36:17 +0530

    fix: improve Stream call join with audio/video, add cleanup, and navigation fixes for CallPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -70,7 +70,13 @@ const App = () => {
          ) : <Navigate to={isAuthenticated ? "/login" : "/onboarding"} />}
         />
         
-        <Route path="/call" element={isAuthenticated ?<CallPage /> : <Navigate to="/login" />} />
+        <Route path="/call/:id" element={
+          isAuthenticated && isOnboarded ? (
+          <CallPage />
+         ) : (
+         <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+        )}
+        />
 
         <Route path="/chat/:id" 
         element={isAuthenticated && isOnboarded ? (
